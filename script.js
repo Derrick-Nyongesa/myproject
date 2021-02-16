59057156203d8f846abc1b7fb741c79a3ad34a08
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-02-16 16:44:16 +0300

    I added the male and femals Akan names into an array

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const myForm = document.getElementById("myForm");
+
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
