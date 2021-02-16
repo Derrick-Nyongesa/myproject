8c95bd0633df59e563a0b595e43af095c935c868
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-02-16 16:49:07 +0300

    Added a function to stop default bahavior of the browser redirecting to a different page

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const myForm = document.getElementById("myForm");
 const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-myForm.addEventListener("submit",(e) => { 
+myForm.addEventListener("submit",(e) => {
+    e.preventDefault();
